b4eada5db782ab164396f2a259d687ec109c7345
Mreren <Pc_ren@test.com>
2020-08-13 11:40:31 +0800

    init

---
+++ reg.bat
@@ -0,0 +1,6 @@
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe /unregister E:\yitiji\ws\repos\bho\ie-test\bin\Debug\wszx.dll
+
+
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe /codebase E:\yitiji\ws\repos\bho\ie-test\bin\Debug\wszx.dll
+
+pause
---
+++ ureg.bat
@@ -0,0 +1,4 @@
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe /unregister E:\yitiji\ws\repos\bho\ie-test\bin\Debug\IEExtension.dll
+
+ 
+pause
